0fe758b52299d9365dd4c86c2dd9c5bb3ea33822
Kacper Wylegała <s9961kw@ms.wwsi.edu.pl>
2023-02-28 07:57:46 +0100

    add score-related fields to db

--- TabelaLigi/Db/Models/Team.cs
+++ TabelaLigi/Db/Models/Team.cs
@@ -1,7 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TabelaLigi.Db.Models;
 
 public class Team
 {
     public int Id { get; set; }
+    [StringLength(40)]
+    [Required]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [Range(0, 100)]
+    public byte MatchesWon { get; set; }
+    [Required]
+    [Range(0, 100)]
+    public byte MatchesLost { get; set; }
+    [Required]
+    [Range(0, 100)]
+    public byte MatchesDrawn { get; set; }
+    [Required]
+    [Range(0, 150)]
+    public int GoalsScored { get; set; }
+    [Required]
+    [Range(0, 150)]
+    public int GoalsLost { get; set; }
+    public int MatchesPlayed => MatchesWon + MatchesLost + MatchesDrawn;
+    public int Points => MatchesWon * 3 + MatchesDrawn;
 }
---
+++ TabelaLigi/Migrations/20230228065211_add-score-fields.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TabelaLigi.Migrations
+{
+    public partial class addscorefields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GoalsLost",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GoalsScored",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "MatchesDrawn",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "MatchesLost",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "MatchesWon",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoalsLost",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "GoalsScored",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "MatchesDrawn",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "MatchesLost",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "MatchesWon",
+                table: "Teams");
+        }
+    }
+}
